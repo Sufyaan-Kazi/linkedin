7d28a6612b09a98c0691d34796267671e97b6962
Sufyaan Kazi <sufyaankazi@google.com>
2018-11-28 21:25:10 +0000

    Updated version that works with LinkedIn

--- src/main/java/com/demo/google/ce/linkedin/controllers/WebController.java
+++ src/main/java/com/demo/google/ce/linkedin/controllers/WebController.java
@@ -2,7 +2,6 @@ package com.demo.google.ce.linkedin.controllers;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,13 +13,12 @@ public class WebController {
     }
 
     /*
-    @RequestMapping(value = "/login", method = RequestMethod.GET)
-    public String doLogin(@RequestParam(value = "code", required = false) String code,
-            @RequestParam(value = "state", required = false) String state) {
-        log("Doing login: " + code + "," + state);
-        return "login";
-    }
-    */
+     * @RequestMapping(value = "/login", method = RequestMethod.GET) public String
+     * doLogin(@RequestParam(value = "code", required = false) String code,
+     * 
+     * @RequestParam(value = "state", required = false) String state) {
+     * log("Doing login: " + code + "," + state); return "login"; }
+     */
 
     private void log(Object o) {
         if (o == null)
