e3ef39a374de09e9afdcc1ab30b2b65a092585fd
Sufyaan Kazi <sufyaankazi@google.com>
2018-11-29 23:59:43 +0000

    Working calling API

--- src/main/java/com/demo/google/ce/linkedin/controllers/WebController.java
+++ src/main/java/com/demo/google/ce/linkedin/controllers/WebController.java
@@ -1,24 +1,38 @@
 package com.demo.google.ce.linkedin.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.oauth2.client.OAuth2RestOperations;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WebController {
+    @Autowired
+    private OAuth2RestOperations restTemplate;
+
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String sayHello() {
         log("Saying hello");
+        String url = "https://api.linkedin.com/v2/me";
+
+        Object obj = restTemplate.getForObject(url, Object.class);
+        if (obj != null) {
+            log(obj.toString());
+        } else {
+            log("got null object back");
+        }
+
+        try {
+
+            Thread.sleep(5000);
+        } catch (Exception e) {
+            // TODO: handle exception
+            log(e);
+        }
+
         return "hello";
     }
-
-    /*
-     * @RequestMapping(value = "/login", method = RequestMethod.GET) public String
-     * doLogin(@RequestParam(value = "code", required = false) String code,
-     * 
-     * @RequestParam(value = "state", required = false) String state) {
-     * log("Doing login: " + code + "," + state); return "login"; }
-     */
 
     private void log(Object o) {
         if (o == null)
